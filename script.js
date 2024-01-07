0ed73de8648a5f6731d993c7ea2650e623327656
jdub <jessicaclaing@gmail.com>
2024-01-06 22:50:08 -0800

    fixed bug so that past/present/future classes properly apply to the correct hours

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
-// let test = () => {
-// $(function () {
+$(document).ready(function () {
 
   //set header to today's date
   let todaysDate = dayjs().format("dddd, MMMM DD");
@@ -24,36 +19,26 @@
     
   //calculate current time and compare to each time slow to set to past, present, or future
   let now = dayjs().hour();
-  console.log(now);
 
   let scheduleEl = $(".container-lg").children(".row");
-  console.log(scheduleEl);
   
   for (let i = 0; i < scheduleEl.length; i++){
     let hourX = $(scheduleEl[i]).attr("id");
     let hourOnly = hourX.slice(5);
-    console.log(hourOnly);  
-
+    let test = now - 10;
+    
     //conditional to assign past/present/future class
-    if (hourOnly < now) {
-      $(scheduleEl[i]).addClass(".past");
-    } else if (hourOnly > now) {
-      $(scheduleEl[i]).addClass(".future");
+    if (hourOnly < test) {
+      $(scheduleEl[i]).removeClass("future");
+      $(scheduleEl[i]).removeClass("present");
+      $(scheduleEl[i]).addClass("past");
+    } else if (hourOnly > test) {
+      $(scheduleEl[i]).addClass("future");
     } else {
-      $(scheduleEl[i]).addClass(".present");
+      $(scheduleEl[i]).removeClass("future");
+      $(scheduleEl[i]).addClass("present");
     }
 
   }
 
-
-
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-    //
-
-//   });
-// }
-// test();
+});
